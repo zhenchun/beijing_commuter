b4b8ce17f1e82b032c54c52b61e2232ca4fd7638
zhenchun <zy037@yahoo.com>
2021-03-26 15:26:26 +0800

    add beijing_communter.R

--- beijing_communter.R
+++ beijing_communter.R
@@ -3,6 +3,89 @@ require(dplyr)
 require(ggplot2)
 require(rcompanion)
 require(pairwiseCI)
+require(readr)
+require(Hmisc)
+require(PerformanceAnalytics)
+
+
+#####functions#################
+filterMDv4<-function(fileTOfilter){
+  #  fileTOfilter<-MDdat2#for testing
+  tempdat0<-fileTOfilter
+  tempdat<-tempdat0[-1,] #first row (1-sec) removed to match output from 'ratioV2'
+  
+  ##SIZE RANGE CHANGES (MING 17-06-2015 email)
+  validSize<-tempdat$Size>10 & tempdat$Size<300
+  validFlow<-tempdat$Flow>=0.9 & tempdat$Flow<=1.1
+  #MD error breakdown:
+  #the following converts the MDerror value into a binary number with 32 places.
+  #this is used to figure out what errors are present by column...
+  md<-as.data.frame(do.call(rbind, lapply(tempdat$Error,function(x){as.integer(intToBits(x))})))
+  names(md)<-c('MDw1','MDw2','MDw3','MDw4','MDw5','gap','MDe1','MDe2','MDe3','MDe4','MDe5','MDe6','MDe7','MDe8','MDe9','MDe10')
+  Errors <- md$MDw1|md$MDw2|md$MDw3|md$MDw4|md$MDw5|md$MDe1|md$MDe2|md$MDe3|md$MDe4|md$MDe5|md$MDe6|md$MDe7|md$MDe8|md$MDe9|md$MDe10 
+  notErrors<-!Errors  
+  validCount<-tempdat$Number>100
+  Factor<-ratioV3(tempdat0$Number,10)  #the # of rows will now match those of other valid* vars.
+  validFactor<-!Factor[,4]
+  keepIndex<-validSize & validFlow & notErrors & validCount & validFactor
+  cleanData<-tempdat[keepIndex,]
+  percOf24hrs<-dim(cleanData)[1]/60/60/24*100
+  stats<-data.frame(orig_n=length(validSize),total_drop=sum(!keepIndex),size_drop=sum(!validSize),flow_drop=sum(!validFlow),Num_drop=sum(!validCount), Factor_drop=sum(!validFactor), error_drop=sum(Errors),perc_total_drop=sum(!keepIndex)/length(validSize)*100,perc_of_24hrs=percOf24hrs,MDw1=sum(md$MDw1),MDw2=sum(md$MDw2),MDw3=sum(md$MDw3),MDw4=sum(md$MDw4),MDw5=sum(md$MDw5),MDe1=sum(md$MDe1),MDe2=sum(md$MDe2),MDe3=sum(md$MDe3),MDe4=sum(md$MDe4),MDe5=sum(md$MDe5),MDe6=sum(md$MDe6),MDe7=sum(md$MDe7),MDe8=sum(md$MDe8),MDe9=sum(md$MDe9),MDe10=sum(md$MDe10))
+  #remove the first row (1-second) of cleanData & stats as
+  return(list(cleanData,stats))
+  
+}
+
+
+
+
+importMD_dm<-function(filename){
+  #LOAD MINIDISC DATA INTO R:
+  dat<-read.csv(file=filename,header=TRUE,skip=5,sep="\t")
+  oldjava<-dim(dat)[2]==16
+  #Following removes extra last column that shows up in dat thru javascript:
+  if(dim(dat)[2]==16)rmcol<-16  #output file processed with earlier javascript 
+  if(dim(dat)[2]==18)rmcol<-18  #output file processed with javascript v1.217.
+  dat<-dat[,-rmcol] #remove extra \t column (turns out that somewhere between season 1&2, the javatool provides 2 additional columns (Surface & Mass); therefore, need to remove column 16 before, and column 18 after new columns introduced...
+  timemd <- strptime(dat[,1],format="%d-%b-%Y %H:%M:%S") #CONVERT TIMESTAMP TO R time standard
+  dat2<-cbind(PosixTime=timemd,dat)
+  return(list(dat2,oldjava))
+}
+
+
+
+
+
+ratioV3<-function(v,x){ #(vector v, factor x)
+  vectorLen<-length(v)
+  fact<-x
+  tempdat0<-v[-vectorLen] #drop the last observation
+  tempdat1<-v[-1]         #drop the first observation
+  ratio<-tempdat1/tempdat0
+  
+  ratio1<-as.data.frame(cbind(ratio,ratio>fact|ratio<1/fact)) #the length of this vector is 1 less than the input
+  
+  
+  cumsum1<-cumsum(ratio1[,2]) #cumulative sum.
+  rle1<-rle(cumsum1) #run length encoding--Compute the lengths and values of runs of equal values
+  tossDF<-as.data.frame(cbind(rle1$values,rle1$lengths)) #dataframe with values and their lengths
+  names(tossDF)<-c('values','lengths')
+  tossDF<-tossDF[-1,] #remove value of '0' from being tossed.
+  index<-which(tossDF$lengths==1) #if a value occurs only once, that means that it is surrounded on both sides by values that are a factor of >10 different.
+  valsToRm<-tossDF$values[index] #using the index, pick out the cumulative sums to be removed.
+  to_remove<-is.element(cumsum1,valsToRm)                     
+  
+  ratio1<-cbind(ratio1,cumsum1,to_remove)
+  names(ratio1)[2]<-paste('GTorLTfact_',fact,sep='')
+  return(ratio1)
+}
+
+
+
+gm_mean<-function(x, na.rm=TRUE){
+  exp(sum(log(x[x > 0]), na.rm=na.rm) / length(x))
+}
+
 
 
 setwd("C:/Users/zy125/Box Sync/PhD/Beijing")
@@ -31,7 +114,7 @@ bus.all = do.call(rbind, bus)
 
 bus_summary<-bus.all %>%
   group_by(id) %>%
-  summarize(a.mean = mean(Number), g.mean=gm_mean(Number),median=median(Number), 
+  dplyr::summarize(a.mean = mean(Number), g.mean=gm_mean(Number),median=median(Number), 
             max=max(Number), min=min(Number), sd=sd(Number),q5=quantile(Number, probs = 0.05),
             q25=quantile(Number, probs = 0.25), q75=quantile(Number, probs = 0.75),
             q95=quantile(Number, probs = 0.95), length=length(Number))
@@ -70,7 +153,7 @@ car.all = do.call(rbind, car)
 
 car_summary<-car.all %>%
   group_by(id) %>%
-  summarize(a.mean = mean(Number), g.mean=gm_mean(Number),median=median(Number), 
+  dplyr::summarize(a.mean = mean(Number), g.mean=gm_mean(Number),median=median(Number), 
             max=max(Number), min=min(Number), sd=sd(Number),q5=quantile(Number, probs = 0.05),
             q25=quantile(Number, probs = 0.25), q75=quantile(Number, probs = 0.75),
             q95=quantile(Number, probs = 0.95), length=length(Number))
@@ -105,7 +188,7 @@ bicycle.all = do.call(rbind, bicycle)
 
 bicycle_summary<-bicycle.all %>%
   group_by(id) %>%
-  summarize(a.mean = mean(Number), g.mean=gm_mean(Number),median=median(Number), 
+  dplyr::summarize(a.mean = mean(Number), g.mean=gm_mean(Number),median=median(Number), 
             max=max(Number), min=min(Number), sd=sd(Number),q5=quantile(Number, probs = 0.05),
             q25=quantile(Number, probs = 0.25), q75=quantile(Number, probs = 0.75),
             q95=quantile(Number, probs = 0.95), length=length(Number))
@@ -140,10 +223,56 @@ for (n in 1:21){
 subway.all = do.call(rbind, subway)
 
 
+
+############################################################################
+###############load info data################################################
+##########################################################################
+
+
+bus_info <- read_csv("C:/Users/zy125/Box Sync/PhD/Beijing/bus_info.csv")
+subway_info <- read_csv("C:/Users/zy125/Box Sync/PhD/Beijing/subway_info.csv")
+car_info <- read_csv("C:/Users/zy125/Box Sync/PhD/Beijing/car_info.csv")
+bicycle_info <- read_csv("C:/Users/zy125/Box Sync/PhD/Beijing/bicycle_info.csv")
+
+
+
+
+
+
+bus_car<-beijing[which(beijing$type=="bus"| beijing$type=="car" ),]
+t.test(g.mean~type, data=bus_car)
+
+
+bus_bicycle<-beijing[which(beijing$type=="bus"| beijing$type=="bicycle" ),]
+t.test(g.mean~type, data=bus_bicycle)
+
+bus_subway<-beijing[which(beijing$type=="bus"| beijing$type=="subway" ),]
+
+t.test(g.mean~type, data=bus_subway)
+
+car_bicycle<-beijing[which(beijing$type=="bicycle"| beijing$type=="car" ),]
+
+t.test(g.mean~type, data=car_bicycle)
+
+car_subway<-beijing[which(beijing$type=="subway"| beijing$type=="car" ),]
+
+t.test(g.mean~type, data=car_subway)
+
+bicycle_subway<-beijing[which(beijing$type=="bicycle"| beijing$type=="subway" ),]
+
+t.test(g.mean~type, data=bicycle_subway)
+
+
+
+
+
+
+
+
 ##############################################################################################################################
 #subway data id==7&12 have small amount of data available. Thus, deleted.id=14 has strange data inclusion. deleted as well.###
 ###############################################################################################################################
-subway.all1<-subway.all[-which(subway.all$id==7 | subway.all$id==12| subway.all$id==14),]
+subway.all1<-subway.all[-which(subway.all$id==7 | subway.all$id==12),]
 
 subway_summary<-subway.all1 %>%
   group_by(id) %>%
@@ -162,32 +291,78 @@ subway_summary_size<-subway.all1 %>%
 ##################combine travel modes#######################################
 ##################################################################################
 
-bus$type<-"bus"
-bus<-cbind(bus_info, bus_summary)
-
-car$type<-"car"
-car<-cbind(car_info, car_summary)
 
 
-bicycle$type<-"bicycle"
-bicycle<-cbind(bicycle_info, bicycle_summary)
+beijing<-as.data.frame(rbind(bus_info, car_info, bicycle_info, subway_info))
 
 
-subway$type<-"subway"
-subway<-cbind(subway_info, subway_summary)
+beijing %>% group_by(type)%>% 
+  dplyr::summarize(n=n())
 
 
-beijing<-rbind(bus, car, bicycle, subway)
+beijing %>% group_by(period)%>% 
+  dplyr::summarize(n=n())
 
+
+
+beijing %>% group_by(pattern)%>% 
+  dplyr::summarize(n=n())
+
+beijing_summary_size<-beijing %>%
+   group_by(type) %>%
+   dplyr::summarize( mean = mean(size.mean),sd=sd(size.mean), max=max(size.mean), min=min(size.mean))
+
+
+
+beijing_cor<-beijing[,c(11,13,14,15,17,20,26)]
+
+bus_cor<-bus[,c(11,13,14,15,17,20,26)]
+
+bicycle_cor<-bicycle[,c(11,12,13,14,15,17,20,26)]
+
+res2 <- rcorr(as.matrix(beijing_cor))
+
+
+outdoor<-rbind(bus, bicycle)
+
+outdoor_cor<-outdoor[,c(11,12,13,14,15,17,20,26)]
+
+res2_outdoor <- rcorr(as.matrix(outdoor_cor))
+
+
+chart.Correlation(beijing_cor, histogram=TRUE)
 ######plot####################################################################################
 
 tiff("Figure1.tiff", units="in", width=8, height=6, res=300)
 
-ggplot(beijing_3, aes( type, mean, fill=pattern))+geom_boxplot()+theme_bw()+
-  scale_fill_manual(values=c("#336699", "#cc9999"))+ 
-  scale_x_discrete(labels= c("Bicycle (n=10)", "Bus (n=18)","Car (n=16)","Subway (n=19)"))+labs(x=NULL, y=expression(paste("UFP Number Concentration (Particle Number / ", cm^3, ")")), fill="Averages Type")+
+ggplot(beijing, aes( x=type, y=g.mean))+geom_boxplot(fill="grey")+theme_bw()+
+  scale_x_discrete(labels= c("Bicycle (n=10)", "Bus (n=18)","Car (n=16)","Subway (n=19)"))+labs(x=NULL, y=expression(paste("UFP Number Concentration (Particle Number /  ", cm^3, ")")), fill="Averages Type")+
   theme(text = element_text(size=14),legend.position="bottom")
 dev.off()
+
+
+
+ggplot(beijing, aes( x=type, y=size.mean))+geom_boxplot(fill="grey")+theme_bw()+
+  scale_x_discrete(labels= c("Bicycle (n=10)", "Bus (n=18)","Car (n=16)","Subway (n=19)"))+labs(x=NULL, y=expression(paste("UFP Number Concentration (Particle Number /  ", cm^3, ")")), fill="Averages Type")+
+  theme(text = element_text(size=14),legend.position="bottom")
+
+
+ggplot(beijing, aes( x=type, y=PM25_30))+geom_boxplot(fill="grey")+theme_bw()+
+  scale_x_discrete(labels= c("Bicycle (n=10)", "Bus (n=18)","Car (n=16)","Subway (n=18)"))+labs(x=NULL, y=expression(paste("UFP Number Concentration (Particle Number /  ", cm^3, ")")), fill="Averages Type")+
+  theme(text = element_text(size=14),legend.position="bottom")
+
+
+
+ggplot(beijing, aes( x=type))+geom_boxplot(aes(y=PM25_30*1000),fill="grey")+geom_boxplot(aes(y=g.mean),fill="grey")+theme_bw()+
+  scale_x_discrete(labels= c("Bicycle (n=10)", "Bus (n=18)","Car (n=16)","Subway (n=18)"))+labs(x=NULL, y=expression(paste("UFP Number Concentration (Particle Number /  ", cm^3, ")")), fill="Averages Type")+
+  theme(text = element_text(size=14),legend.position="bottom")+scale_y_continuous(
+    
+    # Features of the first axis
+    name = "Temperature (Celsius °)",
+    
+    # Add a second axis and specify its features
+    sec.axis = sec_axis(~./100, name="Price ($)")
+  )
 
 
 
@@ -239,9 +414,14 @@ beijing %>%
 
 subway.min.num<-subway.all1 %>%
   group_by(time = cut(PosixTime, breaks="1 min")) %>%
-  dplyr::summarize(con = mean(Number), id2=unique(id))
+  dplyr::summarize(con = gm_mean(Number), id2=unique(id))
 
+
+subway_micro_summary <- read_csv("C:/Users/zy125/Box Sync/PhD/Beijing/subway_micr.csv")
 subway_micro_summary$environment<-factor(subway_micro_summary$environment, levels= c("zgc", "underground", "transfer","overground", "sd") )
+
+
+subway_micro_summary$type<-NA
 
 subway_micro_summary$type[subway_micro_summary$environment== "zgc"|subway_micro_summary$environment== "sd"]<-"Entrance"
 
@@ -250,24 +430,68 @@ subway_micro_summary$type[subway_micro_summary$environment== "underground"|subwa
 subway_micro_summary$type[subway_micro_summary$environment== "transfer"]<-"Tunnel"
 
 
-tiff("Figure2.tiff", units="in", width=8, height=6, res=300)
+subway_micro_sum<-subway_micro_summary %>%
+  group_by(environment, id2) %>%
+  dplyr::summarize(n=n(), pmean = mean(con), pmax=max(con), pmin=min(con), pmedian=median(con))
 
-ggplot(subway_micro_summary, aes(environment,a.mean, fill=type))+geom_boxplot()+theme_bw()+
+
+subway_micro_sum<-subway_micro_summary %>%
+  group_by(environment) %>%
+  dplyr::summarize(n=n(), pmean = mean(con), psd=sd(con),pmax=max(con), pmin=min(con), pmedian=median(con))
+
+
+
+
+zgc_sd<-subway_micro_summary[which(subway_micro_summary$environment=="zgc"| subway_micro_summary$environment=="sd" ),]
+t.test(con~environment, data=zgc_sd)
+
+zgc_over<-subway_micro_summary[which(subway_micro_summary$environment=="zgc"| subway_micro_summary$environment=="overground" ),]
+t.test(con~environment, data=zgc_over)
+
+zgc_under<-subway_micro_summary[which(subway_micro_summary$environment=="zgc"| subway_micro_summary$environment=="underground" ),]
+t.test(con~environment, data=zgc_under)
+
+zgc_transfer<-subway_micro_summary[which(subway_micro_summary$environment=="zgc"| subway_micro_summary$environment=="transfer" ),]
+t.test(con~environment, data=zgc_transfer)
+
+sd_over<-subway_micro_summary[which(subway_micro_summary$environment=="sd"| subway_micro_summary$environment=="overground" ),]
+t.test(con~environment, data=sd_over)
+
+sd_under<-subway_micro_summary[which(subway_micro_summary$environment=="sd"| subway_micro_summary$environment=="underground" ),]
+t.test(con~environment, data=sd_under)
+
+sd_transfer<-subway_micro_summary[which(subway_micro_summary$environment=="sd"| subway_micro_summary$environment=="transfer" ),]
+t.test(con~environment, data=sd_transfer)
+
+over_under<-subway_micro_summary[which(subway_micro_summary$environment=="overground"| subway_micro_summary$environment=="underground" ),]
+t.test(con~environment, data=over_under)
+
+over_transfer<-subway_micro_summary[which(subway_micro_summary$environment=="overground"| subway_micro_summary$environment=="transfer" ),]
+t.test(con~environment, data=over_transfer)
+
+
+under_transfer<-subway_micro_summary[which(subway_micro_summary$environment=="underground"| subway_micro_summary$environment=="transfer" ),]
+t.test(con~environment, data=under_transfer)
+
+
+
+
+tiff("Figure3.tiff", units="in", width=8, height=6, res=300)
+
+ggplot(subway_micro_summary, aes(environment,con, fill=type))+geom_boxplot()+theme_bw()+
   labs(x=NULL, y=expression(paste("UFP Number Concentration (Particle Number / ", cm^3, ")")), fill="Micro-Environments Type")+
   scale_x_discrete(labels= c("ZGC (n=19)", "Underground (n=19)","Transfer (n=19)","Overground (n=19)", "SD (n=19)"))+
   theme(text = element_text(size=14),legend.position="bottom")
 dev.off()
 
 
-subway_micro_summary<-subway_micro %>%
-  group_by(environment, journey) %>%
-  dplyr::summarize(n=n(), a.mean = mean(con), g.mean=gm_mean(con),length=length(con))
+
 
 
 subway_micro_summary_envir<-subway_micro_summary %>%
   group_by(environment) %>%
-  summarize(time=mean(length), amean = mean(a.mean), amax=max(a.mean),
-            amin=min(a.mean),gmean=mean(g.mean),gmax=max(g.mean), gmin=min(g.mean),n=n())
+  dplyr::summarize(length=n(), mean = mean(con),sd=sd(con), max=max(con),
+            min=min(con))
 
 
 
@@ -302,11 +526,13 @@ beijing_1<-rbind(beijing_amean, beijing_gmean)
 
 pairwiseCI(a.mean~type, data=commuter, method="Param.diff")
 
-ci.ratio<-pairwiseCI(a.mean~type, data=commuter, method="Param.ratio")
+ci.ratio<-pairwiseCI(g.mean~type, data=beijing, method="Param.ratio")
 
 write.csv(ci.ratio, "ci.ratio.csv")
 
+micro_ci_ratio<-pairwiseCI(con~environment, data=subway_micro_summary, method="Param.ratio")
 
+write.csv(micro_ci_ratio, "micro_ci_ratio.csv")
 pd <- position_dodge(width = 1)
 
 
@@ -401,8 +627,22 @@ ggplot(sum_ratio, aes(x = Mean, y = type))+geom_errorbar(aes(xmin = Trad.lower,
 
 
 
+#########################temporal adjusted######################################
+########################################################################################
+ggplot(trip_temp, aes(type, a.mean.adj))+geom_boxplot()+theme_bw()+
+  scale_fill_manual(values=c("#336699", "#cc9999"))+ 
+  scale_x_discrete(labels= c("Bicycle (n=10)", "Bus (n=18)","Car (n=16)","Subway (n=19)"))+labs(x=NULL, y=expression(paste("Temporal Adjusted Arithmetic Averages of UFP Number Concentration (n/  ",  cm^3,")")))+
+  theme( text = element_text(size=16), axis.title.y = element_text(size=14, vjust=0.5, margin = margin(0, 0, 0, 10)))+ theme_bw()+theme(plot.margin=unit(c(1,2,1,2),"cm"))
 
 
+############################################################################################
+######################################################################################
 
 
+tiff("Figure2.tiff", units="in", width=8, height=6, res=300)
+ggplot(temporal_adjusted, aes(type, number, fill=method))+geom_boxplot()+theme_bw()+
+  scale_fill_manual(labels = c("Temporal Adjusted Measurements", "Real Time Measurements"), values=c("#336699", "#cc9999"))+ 
+  scale_x_discrete(labels= c("Bicycle (n=10)", "Bus (n=18)","Car (n=16)","Subway (n=19)"))+labs(x=NULL, y=expression(paste("UFP Number Concentration (n/ ",  cm^3,")")))+
+  theme( text = element_text(size=16), axis.title.y = element_text(size=14, vjust=0.5, margin = margin(0, 0, 0, 2)))+ theme_bw()+theme(plot.margin=unit(c(1,2,1,2),"cm"))+theme(legend.title = element_blank(),legend.position = "bottom")
 
+dev.off()
